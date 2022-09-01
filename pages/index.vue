c350aac37618c5daed480b4003e19965d64f0121
SergeyKovalchuk2086 <kovalchuk.s.v@inbox.ru>
2022-09-01 13:48:17 +0700

    configured common rules in prettier and eslint

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Tutorial />
+    <div class="temp">Lorem1</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      temp: 'test',
+      temp2: '231'
+    }
+  },
+  methods: {
+    tempe() {
+      console.log(1)
+    }
+  }
+})
+</script>
